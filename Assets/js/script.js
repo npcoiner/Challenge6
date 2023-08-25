1fa1896e58eeb945af4e4c446e1acc952b7bdc18
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-08-24 18:28:28 -0700

    Added display functionality in displayWeatherData

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,6 +8,9 @@ var cityInputEl = document.querySelector('#city');
 var weatherContainerEl = document.querySelector('#weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
 
+var cityNameEl = $('#city-name');
+
+var weatherData = $('#weather-data');
 
 var formSubmitHandler = function (e) {
     e.preventDefault();
@@ -36,7 +39,8 @@ function getCityWeather(city){
         if (response.ok) {
             response.json().then(function (data){
                 var city=  data[0];
-                var apiUrl2 = 'https://api.openweathermap.org/data/2.5/forecast?lat='+city.lat+'&lon='+city.lon+'&appid='+apiKey;
+                var apiUrl2 = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat='+city.lat+'&lon='+city.lon+'&appid='+apiKey;
+                console.log(apiUrl2);
                 fetch(apiUrl2).then(function(nestedResponse){
                     if (nestedResponse.ok) {
                         nestedResponse.json().then(function (data2){
@@ -55,8 +59,31 @@ function getCityWeather(city){
 
 //This code displays the weather data from getCityWeather.
 //Should only be called from getCityWeather
-function displayWeatherData(city){
+function displayWeatherData(data){
+    cityNameEl.html(data.city.name);
+    console.log("HELLO?????"+ cityNameEl.text());
+    weatherData.html("");
+    var htmlString = "";
+    var day = 0;
+    var dayData = null;
+    for (var i = 7; i < 47; i += 8) {
+        dayData = data.list[i];
+        day++;
 
+        htmlString += `
+        <h3 class='card-header text-uppercase weather-day'>
+        Day `+day+`
+        </h3>
+        <div class='card-body'>
+        <div class='form-label'>Date&Time: `+dayData.sys.dt_txt+`</div>
+        <div class='form-label'>Weather: `+dayData.weather[0].main+`</div>
+        <div class='form-label'>Temp: `+dayData.main.temp+`</div>
+        <div class='form-label'>`+1+`</div>
+        <div class='form-label'>`+1+`</div>
+        </div>
+        `;
+    }
+    weatherData.html(htmlString);
 }
 
 citySearchFormEl.addEventListener('submit', formSubmitHandler);
